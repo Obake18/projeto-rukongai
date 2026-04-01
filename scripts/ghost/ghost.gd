eb1cf548e957ae3d10b3ce2149963863b887bea5
Obake18 <coimbrajuniorm@gmail.com>
2026-04-01 17:26:48 -0300

    Criado o gerador de encontros aleatórios
    Implementado o protótipo dos primeiros fantasmas
    
    Rascunho do personagem

---
+++ scripts/ghost/ghost.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var youkai_data: Youkai
+
+var triggered := false  # evita repetir o evento
+
+func _ready():
+    # conecta o sinal da área
+    $Area3D.body_entered.connect(_on_body_entered)
+    if youkai_data == null:
+        youkai_data = YoukaiManager.youkais.pick_random()
+
+func _on_body_entered(body):
+    if triggered:
+        return
+
+    if body.is_in_group("player"):
+        triggered = true
+        start_encounter()
+
+func start_encounter():
+    print("Encontro iniciado com: ", youkai_data.name)
+    get_tree().change_scene_to_file("res://scenes/battle.tscn")
+
---
+++ scripts/youkai_manager.gd
@@ -0,0 +1,1 @@
+YoukaiManager.youkais
