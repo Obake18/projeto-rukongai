a073259b94d6561786fe96f7647f921a2eaa9613
Obake18 <coimbrajuniorm@gmail.com>
2026-04-05 21:03:06 -0300

    Feito o chão, e o sistema de evoca fantasmas.

---
+++ scripts/systems/youkai_spawner.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var ghost_scene: PackedScene
+@export var spawn_count := 5
+@export var spawn_area := Vector3(20, 0, 20)
+
+func _ready():
+    spawn_ghosts()
+
+func spawn_ghosts():
+    for i in spawn_count:
+        var ghost = ghost_scene.instantiate()
+
+        var random_pos = Vector3(
+            randf_range(-spawn_area.x, spawn_area.x),
+            1,
+            randf_range(-spawn_area.z, spawn_area.z)
+        )
+
+        ghost.position = random_pos
+        add_child(ghost)
